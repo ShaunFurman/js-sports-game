1eb6d33672f764b5537c43de9464d939490ee59f
Shaun Furman <shaunfurman08@gmail.com>
2020-05-13 22:15:42 -0500

    finished product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,49 @@
 let teamoneshootButton = document.querySelector("#teamoneshootbutton"); 
 let teamtwoshootButton = document.querySelector("#teamtwoshootbutton");
-let resetbutton = document.querySelector("#resetbutton");  
-let counter = document.querySelector("#teamone-numshots");
+    let teamonegoals = document.querySelector("#teamone-numgoals");
+    let teamtwogoals = document.querySelector("#teamtwo-numgoals");
+let counter1 = document.querySelector("#teamone-numshots");
 let counter2 = document.querySelector("#teamtwo-numshots");
 let counter3 = document.querySelector("#num-resets");
-
+    let resetbutton = document.querySelector("#reset-button"); 
 
 
 
 teamoneshootButton.addEventListener("click", function() {
     console.log("+ button clicked");
-    let newCounterValue = Number(counter.innerHTML) + 1;
+    let newCounterValue = Number(counter1.innerHTML) + 1;
+    counter1.innerHTML = newCounterValue
+    if (Math.random() > .5) {
+        let newGoals = Number(teamonegoals.innerHTML) + 1
+        teamonegoals.innerHTML = newGoals
+      }
+            console.log("+ Random number");
 
-counter.innerHTML = newCounterValue;
 })
-// document.getElementById("teamone-numgoals").innerHTML =
-// Math.floor(Math.random() * 10);
-
 //TWO
 
 teamtwoshootButton.addEventListener("click", function() {
     console.log("+ button clicked");
     let newCounterValue = Number(counter2.innerHTML) + 1;
-
-counter2.innerHTML = newCounterValue;
+    counter2.innerHTML = newCounterValue;
+// if random chance suceed then increment GOALS like SHOTS 
+  if (Math.random() > .5) {
+    let newGoals = Number(teamtwogoals.innerHTML) + 1
+    teamtwogoals.innerHTML = newGoals
+  }
+        console.log("+ Random number");
+    //----------------> new 5/10/20
 })
-// document.getElementById("teamtwo-numgoals").innerHTML =
-// Math.floor(Math.random() * 10);
 
 //RESET
 
 resetbutton.addEventListener("click", function() {
-    console.log("+ button clicked");
-    let newCounterValue = Number(counter3.innerHTML) + 1;
-    let teamoneshootbutton; count= 0
-    let teamtwoshootbutton; count= 0
-counter3.innerHTML = newCounterValue;
-})
+    console.log("+ reset clicked");
 
+    let newCounterValue = Number(counter3.innerHTML) + 1;
+    counter3.innerHTML = newCounterValue;
+    counter1.innerHTML = 0;
+    counter2.innerHTML = 0;
+    teamonegoals.innerHTML = 0;
+    teamtwogoals.innerHTML = 0;
+})
